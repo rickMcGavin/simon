8de0958c48738872117993558498a36106481b48
rebelBeard <rfm144@gmail.com>
2017-02-08 23:32:08 -0500

    Simon game: fully functional

--- js/simon.js
+++ js/simon.js
@@ -1,221 +1,242 @@
-var powerOn = false;
-var switchSlot = document.querySelector(".slot");
-var powerSwitch = document.querySelector(".switch");
-var strictButton = document.querySelector(".strict");
-var strictLight = document.querySelector(".led-light");
-var startButton = document.querySelector(".start");
-var display = document.querySelector(".display");
-var match = true;
+(function() {
 
-// game buttons
-var gameButtons = document.getElementsByClassName("game-btn");
-
-// green button
-var greenButton = document.querySelector(".top-left");
-var greenLightOn = "#13FF7C";
-var greenLightOff = "#00A74A";
-
-// red button
-var redButton = document.querySelector(".top-right");
-var redButtonOn = "#FF4C4C";
-var redButtonOff = "#9F0F17";
-
-// yellow button
-var yellowButton = document.querySelector(".bottom-left");
-var yellowButtonOn = "#FED938";
-var yellowButtonOff = "#CCA707";
-
-// blue button
-var blueButton = document.querySelector(".bottom-right");
-var blueButtonOn = "#1C8CFF";
-var blueButtonOff = "#094A8F";
-
-// move arrays
-var computerMoves = [];
-var playerMoves = [];
-var computerTurn = true;
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * 4);
-}
-
-function updateDisplay(count) {
-  count <= 9 ? display.textContent = "0" + count : display.textContent = count;
-}
-
-function moveLooper(count) {
-  count = count || 0;
-  setInterval(function() {
-    if (count <= computerMoves.length) {
-      lightUpButton(computerMoves[count]);
-      count++;
-      moveLooper(count);
-    }
-  }, 500);
-}
+  // declare selectors
+  const switchSlot = document.querySelector(".slot");
+  const powerSwitch = document.querySelector(".switch");
+  const strictButton = document.querySelector(".strict");
+  const strictLight = document.querySelector(".led-light");
+  const startButton = document.querySelector(".start");
+  const display = document.querySelector(".display");
 
 
-function playSound(color) {
-  let audio = document.querySelector("."+color);
-  audio.currentTime = 0;
-  audio.play();
-}
+  // game buttons
+  const gameButtons = document.querySelectorAll(".game-btn");
 
-function lightUpButton(color) {
-  var audioFile;
-  switch(color) {
-    case "green":
-        greenButton.style.background = greenLightOn;
-        playSound("green");
-        setTimeout(function() {
-          greenButton.style.background = greenLightOff;
-        }, 250);
-      break;
-    case "red":
-        redButton.style.background = redButtonOn;
-        playSound("red");
-        setTimeout(function() {
-          redButton.style.background = redButtonOff;
-        }, 250);
-      break;
-    case "yellow":
-        yellowButton.style.background = yellowButtonOn;
-        playSound("yellow");
-        setTimeout(function() {
-          yellowButton.style.background = yellowButtonOff;
-        }, 250);
-      break;
-    case "blue":
-        blueButton.style.background = blueButtonOn;
-        playSound("blue");
-        setTimeout(function() {
-        blueButton.style.background = blueButtonOff;
-      }, 250);
-      break;
+  // green button
+  const greenLightOn = "#13FF7C";
+  const greenLightOff = "#00A74A";
+
+  // red button
+  const redButtonOn = "#FF4C4C";
+  const redButtonOff = "#9F0F17";
+
+  // yellow button
+  const yellowButtonOn = "#FED938";
+  const yellowButtonOff = "#CCA707";
+
+  // blue button
+  const blueButtonOn = "#1C8CFF";
+  const blueButtonOff = "#094A8F";
+
+  // move arrays
+  var computerMoves = [];
+  var playerMoves = [];
+
+  // Booleans
+  var powerOn = false;
+  var computerTurn = true;
+  var match = true;
+
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 4);
   }
 
-  
-}
-
-function pushComputerMoveToArray(buttonId) {
-  switch(buttonId) {
-    case 0:
-      computerMoves.push("green");
-      break;
-    case 1:
-      computerMoves.push("red");
-      break;
-    case 2:
-      computerMoves.push("yellow");
-      break;
-    case 3:
-      computerMoves.push("blue");
-      break;
+  function updateDisplay(count) {
+    count <= 9 ? display.textContent = "0" + count : display.textContent = count;
   }
-}
 
-function computerMove() {
-  var randomNum = getRandomNumber();
-  updateDisplay(computerMoves.length + 1);
-  pushComputerMoveToArray(randomNum);
-  moveLooper(0);
-  makeButtonsClickable();
-  computerTurn = false;
-  console.log(computerMoves);
-}
-
-function makeButtonsClickable() {
-  greenButton.classList.add("clickable");
-  redButton.classList.add("clickable");
-  yellowButton.classList.add("clickable");
-  blueButton.classList.add("clickable");
-}
-
-
-
-// POWER SWITCH
-
-powerSwitch.addEventListener("click", function() {
-  if (!powerOn) {
-    display.style.color = "#DC0D29";
-    switchSlot.style.justifyContent = "flex-end";
-    powerOn = true;
-  } else {
-    display.style.color = "#430710";
-    switchSlot.style.justifyContent = "flex-start";
-    display.innerText = "--"
-    powerOn = false;
-  }
-});
-
-// START BUTTON
-
-startButton.addEventListener("mousedown", function() {
-  startButton.style.boxShadow = "0px 0px 0px #222";
-});
-
-startButton.addEventListener("mouseup", function() {
-  startButton.style.boxShadow = "0px 2px 3px #222";
-  powerOn ? computerMove() : console.log("Turn on the game");
-})
-
-// STRICT BUTTON
-
-strictButton.addEventListener("mousedown", function() {
-  strictButton.style.boxShadow = "0px 0px 0px #222";
-});
-
-strictButton.addEventListener("mouseup", function() {
-  strictButton.style.boxShadow = "0px 2px 3px #222";
-  if (powerOn) {
-    if (strictLight.classList.contains("led-off")) {
-      strictLight.classList.remove("led-off");
-      strictLight.classList.add("led-on");
-    } else {
-      strictLight.classList.remove("led-on");
-      strictLight.classList.add("led-off");
+  function setDelay(round) {
+     if (round < 5) {
+      return 1000;
+    } else if ((round >= 5) && (round < 9)) {
+      return 750;
+    } else if ((round >= 9) && (round < 13)) {
+      return 500;
+    } else if ((round >= 13) && (round <=20)) {
+      return 400;
     }
   }
-});
 
-function checkIfMoveMatches() {
-  var index = playerMoves.length - 1;
-  if (playerMoves[index] !== computerMoves[index]) {
-    playSound("wrong");
-    match = false;
-    if (strictLight.classList.contains("led-off")) {
-      playerMoves = [];
-      setTimeout(moveLooper, 1500);
-    } 
-    if (strictLight.classList.contains("led-on")) {
-      playerMoves = [];
-      computerMoves = [];
-      setTimeout(computerMove, 1500);
+  function moveLooper(count) {
+    count = count || 0;
+    // determine delay based on round
+    var delay = setDelay(computerMoves.length);
+
+    setTimeout(function() {
+      if (count <= computerMoves.length) {
+        lightUpButton(computerMoves[count]);
+        count++;
+        moveLooper(count);
+      }
+    }, delay);
+    if (count === computerMoves.length) {
+      toggleClickable();
     }
-  } else {
-    match = true;
   }
-}
 
 
-function playerMoveHandler() {
-  if (!computerTurn) {
-    lightUpButton(this.id);
-    playerMoves.push(this.id);
-    checkIfMoveMatches();
-    if ((playerMoves.length === computerMoves.length) && (match)) {
-      if (playerMoves.length === 20) {
-        alert("You Win!");
-      } else {
-        console.log("same lengths");
+  function playSound(sound) {
+    let audio = document.querySelector("."+sound);
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  function lightUpButton(color) {
+    switch(color) {
+      case "green":
+          gameButtons[0].style.background = greenLightOn;
+          playSound("green");
+          setTimeout(() => { gameButtons[0].style.background = greenLightOff }, 150);
+        break;
+      case "red":
+          gameButtons[1].style.background = redButtonOn;
+          playSound("red");
+          setTimeout(() => { gameButtons[1].style.background = redButtonOff }, 150);
+        break;
+      case "yellow":
+          gameButtons[2].style.background = yellowButtonOn;
+          playSound("yellow");
+          setTimeout(() => { gameButtons[2].style.background = yellowButtonOff }, 150);
+        break;
+      case "blue":
+          gameButtons[3].style.background = blueButtonOn;
+          playSound("blue");
+          setTimeout(() => { gameButtons[3].style.background = blueButtonOff}, 150);
+        break;
+    }
+
+    
+  }
+
+  function pushComputerMoveToArray(buttonId) {
+    switch(buttonId) {
+      case 0:
+        computerMoves.push("green");
+        break;
+      case 1:
+        computerMoves.push("red");
+        break;
+      case 2:
+        computerMoves.push("yellow");
+        break;
+      case 3:
+        computerMoves.push("blue");
+        break;
+    }
+  }
+
+
+  function toggleClickable() {
+    gameButtons.forEach(button => button.classList.toggle("unclickable"));
+  }
+
+
+  function computerMove() {
+    toggleClickable();
+    let randomNum = getRandomNumber();
+    updateDisplay(computerMoves.length + 1);
+    pushComputerMoveToArray(randomNum);
+    moveLooper(0);
+    computerTurn = false;
+    console.log(computerMoves);
+  }
+
+
+
+  function checkIfMoveMatches() {
+    var index = playerMoves.length - 1;
+    if (playerMoves[index] !== computerMoves[index]) {
+      match = false;
+      playSound("wrong");
+      if (!strictLight.classList.contains("led-on")) {
         playerMoves = [];
-        computerTurn = true;
-       setTimeout(computerMove, 1000)
+        setTimeout(moveLooper, 1500);
+      } 
+      if (strictLight.classList.contains("led-on")) {
+        playerMoves = [];
+        computerMoves = [];
+        setTimeout(computerMove, 1500);
+      }
+    } else { 
+      match = true;
+    }
+  }
+
+
+  function playerMoveHandler() {
+    if (!computerTurn) {
+      lightUpButton(this.id);
+      playerMoves.push(this.id);
+      checkIfMoveMatches();
+      if ((playerMoves.length === computerMoves.length) && (match)) {
+        if (playerMoves.length === 20) {
+          alert("You Win!");
+          reset();
+        } else {
+          playerMoves = [];
+          computerTurn = true;
+         setTimeout(computerMove, 1000)
+        }
       }
     }
   }
-}
 
-for (var i = 0; i < gameButtons.length; i++) {
-  gameButtons[i].addEventListener("click", playerMoveHandler);
-}
+  function reset() {
+    playerMoves = [];
+    computerMoves = [];
+    toggleClickable();
+    strictLight.classList.remove("led-on");
+  }
+
+  /////////////////////////////
+  // BUTTON EVENT LISTENERS //
+  ////////////////////////////
+
+
+  // POWER SWITCH
+
+  powerSwitch.addEventListener("click", function() {
+    if (!powerOn) {
+      display.style.color = "#DC0D29";
+      switchSlot.style.justifyContent = "flex-end";
+      powerOn = true;
+    } else {
+      display.style.color = "#430710";
+      switchSlot.style.justifyContent = "flex-start";
+      display.innerText = "--"
+      powerOn = false;
+      reset();
+    }
+  });
+
+  // START BUTTON
+
+  startButton.addEventListener("mousedown", function() {
+    startButton.style.boxShadow = "0px 0px 0px #222";
+  });
+
+  startButton.addEventListener("mouseup", function() {
+    startButton.style.boxShadow = "0px 2px 3px #222";
+    powerOn ? computerMove() : console.log("Turn on the game");
+  })
+
+  // STRICT BUTTON
+
+  strictButton.addEventListener("mousedown", function() {
+    strictButton.style.boxShadow = "0px 0px 0px #222";
+  });
+
+  strictButton.addEventListener("mouseup", function() {
+    strictButton.style.boxShadow = "0px 2px 3px #222";
+    if (powerOn) {
+        strictLight.classList.toggle("led-on");;
+      }
+  });
+
+
+
+  // loop through 4 color buttons to set event handlers
+    gameButtons.forEach(button => button.addEventListener("click", playerMoveHandler));
+
+    })();
